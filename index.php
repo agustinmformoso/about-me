73f6a4eb6bf89c714b60d893793f50b655dcca58
agustinmariano.formoso@mercadolibre.com <agustinmariano.formoso@mercadolibre.com>
2022-11-22 23:51:02 -0300

    Basic layout added

--- index.php
+++ index.php
@@ -8,8 +8,20 @@
     <title>About Me</title>
 
     <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/9939576bbf.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <h1>Hello world</h1>
+    <nav class="navbar">
+        <ul class="navbar__ul">
+            <li class="navbar__li"><a href="#"><i class="fa-solid fa-house fa-xl navbar__icon"></i></a></li>
+            <li class="navbar__li"><a href="#"><i class="fa-solid fa-user fa-xl navbar__icon"></i></a></li>
+            <li class="navbar__li"><a href="#"><i class="fa-solid fa-gear fa-xl navbar__icon"></i></a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h1>Sections</h1>
+    </main>
 </body>
 </html>
